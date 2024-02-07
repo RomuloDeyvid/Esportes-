bac3ff69be26293cafe21eb0fdba817479fbc950
izaque silva <izaquek16@hotmail.com>
2024-02-07 18:30:51 -0300

    atualiza
    atualizando as pagina

--- js/carrinho.js
+++ js/carrinho.js
@@ -56,3 +56,69 @@ class ValorUnidades {
 const contadorShort = new ValorUnidades(".Short .btnMais", ".Short .btnMenos", "#quantidadeShort");
 const contadorCamisa = new ValorUnidades(".Camisa .btnMais", ".Camisa .btnMenos", "#quantidadeCamisa");
 const contadorCamiseta = new ValorUnidades(".Camiseta .btnMais", ".Camiseta .btnMenos", "#quantidadeCamiseta");
+
+
+class ValorEntrada {
+
+    constructor (entrada, circulo, corPreta, corAzul, corBranca){
+        this.circuloShort = document.querySelector(circulo);
+        this.pintaPreto = document.querySelector(corPreta);
+        this.pintaAzul = document.querySelector(corAzul);
+        this.PintaBranco = document.querySelector(corBranca);
+        this.entradaTexto = document.querySelector(entrada);
+        this.clickSeleciona();
+    }
+
+    clickSeleciona(){
+
+        this.pintaPreto.addEventListener('click', ()=> {
+            this.circuloShort.style.backgroundColor = '#000';
+            this.entradaTexto.value = this.pintaPreto.textContent
+        });
+
+        this.pintaAzul.addEventListener('click', ()=> {
+            this.circuloShort.style.backgroundColor = 'blue';
+            this.entradaTexto.value = this.pintaAzul.textContent
+        });
+
+        this.PintaBranco.addEventListener('click', ()=> {
+            this.circuloShort.style.backgroundColor = '#fff';
+            this.entradaTexto.value = this.PintaBranco.textContent
+        });
+    }
+
+}
+
+const circuloCamiseta = new ValorEntrada("#corCamiseta", ".corBackCamiseta", ".camisetaPreta", ".camisetaAzulMarinho", ".camisetaBranca");
+const circuloShort = new ValorEntrada("#corShort", ".corBackShort", ".shortPreto", ".shortAzulMarinho", ".shortBranco");
+
+class ValorTamanho {
+
+    constructor (entrada, tamanhoP, tamanhoM, tamanhoG){
+        this.entradaTexto = document.querySelector(entrada);
+        this.pequeno = document.querySelector(tamanhoP);
+        this.medio = document.querySelector(tamanhoM);
+        this.grande = document.querySelector(tamanhoG);
+        this.clickSeleciona();
+    }
+
+    clickSeleciona(){
+
+        this.pequeno.addEventListener('click', ()=> {
+            this.entradaTexto.value = "P"
+        });
+
+        this.medio.addEventListener('click', ()=> {
+            this.entradaTexto.value = "M"
+        });
+
+        this.grande.addEventListener('click', ()=> {
+            this.entradaTexto.value = "G"
+        });
+    }
+
+}
+
+const tamanhoShort = new ValorTamanho("#tamanhoShort", ".tamanhoShortPequeno", ".tamanhoShortMedio", ".tamanhoShortGrande");
+const tamanhoCamiseta = new ValorTamanho("#tamanhoCamiseta", ".tamanhoCamisetaPequena", ".tamanhoCamisetaMedia", ".tamanhoCamisetaGrande");
+const tamanhoCamisa = new ValorTamanho("#tamanhoCamisa", ".tamanhoCamisaPequena", ".tamanhoCamisaMedia", ".tamanhoCamisaGrande");
